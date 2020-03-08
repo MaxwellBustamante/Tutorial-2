94efc457bb8b1feab3f7704f82a58d18746b347f
MaxwellBustamante <59800680+MaxwellBustamante@users.noreply.github.com>
2020-03-08 13:57:23 -0400

    Challenge 2 Additions

---
+++ Tutorial 2 DIG 3480/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationScript : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
--- Tutorial 2 DIG 3480/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2 DIG 3480/Assets/Scripts/PlayerScript.cs
@@ -5,45 +5,119 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    private Rigidbody2D rd2d;
+
     public float speed;
     public Text score;
-    private int scoreValue = 0;
+    public Text winText;
+    public Text livesText;
 
-    // Start is called before the first frame update
+    private Rigidbody2D rd2d;
+    private int scoreValue = 0;
+    private int lives = 3;
+
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+
+    private bool facingRight = true;
+    Animator anim;
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        SetLivesText();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Coin")
         {
-            scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScore();
+            if (scoreValue == 4)
+            {
+                lives = 3;
+                SetLivesText();
+                transform.position = new Vector2(60.0f, -1.61f);
+            }
             Destroy(collision.collider.gameObject);
         }
+        else if (collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            Destroy(collision.collider.gameObject);
+            SetLivesText();
+        }
 
+        if (scoreValue == 10)
+        {
+            musicSource.Stop();
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            musicSource.loop = false;
+            winText.text = "You Win! Game created by Maxwell Bustamante";
+            Destroy(this);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rd2d.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
             }
         }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "You Lose! Game created by Maxwell Bustamante";
+            Destroy(this);
+        }
+    }
+    void SetScore()
+    {
+        scoreValue += 1;
+        score.text = scoreValue.ToString();
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
